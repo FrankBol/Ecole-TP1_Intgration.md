2aaa47eea3645221cb0073011b2d9a428fefd77f
Gabriel Lafrance <gl.gabriellafrance@gmail.com>
2022-03-18 16:46:28 -0400

    Premier commit

--- static/js/table.js
+++ static/js/table.js
@@ -3,6 +3,6 @@ let tr = document.getElementsByTagName("tr");
 for(let i = 0 ; i<tr.length ;i++ ){
     console.log(i%2);
     if(i%2 == 0){
-        tr[i].style.backgroundColor = "red"
+        tr[i].style.backgroundColor = "rgb(231, 140, 21, 0.5)";
     }
 }
