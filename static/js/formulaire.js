6bbf179effc39c7427a169822837294182696c4c
Francis Bolduc <f.bolduc@outlook.com>
2022-03-18 13:34:24 -0400

    premier commit

--- static/js/formulaire.js
+++ static/js/formulaire.js
@@ -3,28 +3,28 @@ let email = document.querySelector("#email");
 
 const regexmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-email.addEventListener("blur", validEmail);
+email.addEventListener("keydown", validEmail);
 
 function validEmail(){
     if(regexmail.test(email.value)){
-        email.classList.remove("emailRouge")
-        email.classList.add("emailVert")
+        email.classList.remove("emailRouge");
+        email.classList.add("emailVert");
     }else{
-        email.classList.remove("emailVert")
-        email.classList.add("emailRouge")
+        email.classList.remove("emailVert");
+        email.classList.add("emailRouge");
     }
 
 }
-body.addEventListener("afterprint", printform);
+// body.addEventListener("afterprint", printform);
 
-function printform(){
-    body{
-        body.classList.add("printForm")
-    }
-}
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+// function printform(){
+//     body{
+//         body.classList.add("printForm")
+//     }
+// }
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
     
-  }
+//   }
 
   
