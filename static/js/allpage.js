6b8546ed33a33166c78d616f9c0c62e8c9371758
Francis Bolduc <f.bolduc@outlook.com>
2022-03-21 23:47:38 -0400

    hover mis en place plus regler probleme header

--- static/js/allpage.js
+++ static/js/allpage.js
@@ -1,3 +1,5 @@
 let loupe = document.querySelector("#loupe");
 document.querySelector("#search").addEventListener("focus", () => loupe.style.display = "none");
 document.querySelector("#search").addEventListener("blur", () => loupe.style.display = "");
+
+logo = () => window.location.assign("/");
