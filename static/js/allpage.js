aa32d25d31d04fb3e3ad23f416dec923871d782f
Francis Bolduc <f.bolduc@outlook.com>
2022-03-23 12:53:49 -0400

    final

--- static/js/allpage.js
+++ static/js/allpage.js
@@ -3,3 +3,10 @@ document.querySelector("#search").addEventListener("focus", () => loupe.style.di
 document.querySelector("#search").addEventListener("blur", () => loupe.style.display = "");
 
 logo = () => window.location.assign("/");
+
+
+let header = document.querySelector("header")
+
+if(window.location.pathname == ("/gin")){
+    header.classList.add("nonePrint");
+ }
